c47f3fef607a299d51a2a30bd24a4f3dde02990a
Victor Miguel <vmmc5910@gmail.com>
2024-05-31 15:50:29 -0300

    Add initial code to deal with state change when the 'Edit' button is clicked.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 export default function Player({name, symbol}){
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick(){
+    setIsEditing(!isEditing);
+  }
+  
   return (
     <li>
       <span className="player">
-        <span className="player-name"> {name} </span>
+        {isEditing ? <input type="text" required/> : <span className="player-name"> {name} </span>}
         <span className="player-symbol"> {symbol} </span>
       </span>
-      <button> Edit </button>
+      <button onClick={handleEditClick}> Edit </button>
     </li>
   );
 }
