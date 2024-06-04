658b5a441f93db80dbd9ef7b56c914ad484222cf
Victor Miguel <vmmc5910@gmail.com>
2024-06-04 19:11:56 -0300

    Add support inside the 'Player' component to edit and save the name of the player.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}){
+export default function Player({initialName, symbol}){
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick(){
     setIsEditing((isEditing) => !isEditing); // Always working with the latest updated state of this component.
   }
+
+  function handleTextChange(event){
+    setPlayerName(event.target.value); // This state update does not depend on a previous state. Thus, there's no need to pass a function to this state-update function.
+  }
   
   return (
     <li>
       <span className="player">
-        {isEditing ? <input type="text" required value={name} /> : <span className="player-name"> {name} </span>}
+        {isEditing ? <input type="text" required value={playerName} onChange={handleTextChange} /> : <span className="player-name"> {playerName} </span>}
         <span className="player-symbol"> {symbol} </span>
       </span>
       <button onClick={handleEditClick}> {isEditing ? "Save" : "Edit"} </button>
