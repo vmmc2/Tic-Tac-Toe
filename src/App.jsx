2910230ea53bf746551f98495915ada16fc58887
Victor Miguel <vmmc5910@gmail.com>
2024-06-06 15:03:35 -0300

    Refactor the 'GameBoard' and 'App' components of our project, so it follow the best practices when it comes to a React project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 import { useState } from "react";
 
-import GameBoard from "./components/GameBoard"
+import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 import Player from "./components/Player";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare(){
+  function handleSelectSquare(rowIndex, colIndex){
     setActivePlayer((currentActivePlayer) => currentActivePlayer === "X" ? "O" : "X");
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X"; // We assume that we are in an odd turn.
+
+      if(prevTurns.length > 0 && prevTurns[0].player === "X"){ // Make a check to see if its necessary to change the value of "currentPlayer".
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        {
+          player: currentPlayer, // We could use "activePlayer" here. However, this is a bad idea because we would be merging different states and by doing this, we have no guarantee that "activePlayer" here is, indeed, the player that is about to make a move on the current turn.
+          square: {
+            row: rowIndex,
+            col: colIndex,
+          }
+        },
+        ...prevTurns
+      ]; // The latest turn info will be displayed first in this list.
+
+      return updatedTurns;
+    });
   }
 
   return (
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,23 +1,21 @@
-import { useState } from 'react';
-
 const initialGameBoard = [ // The game board is represented as a list whose elements are also lists that always have 3 elements.
   [null, null, null], // row
   [null, null, null], // row
   [null, null, null], // row
 ];
 
-export default function GameBoard({ activePlayerSymbol, onSelectSquare }){
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare }){
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-  function handleSelectSquare(rowIndex, colIndex){
-    setGameBoard((previousGameBoard) => {
-      const updatedGameBoard = [...previousGameBoard.map(innerArray => [...innerArray])]; // Creating a deep copy of the previous game board.
-      updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedGameBoard;
-    });
+  // function handleSelectSquare(rowIndex, colIndex){
+  //   setGameBoard((previousGameBoard) => {
+  //     const updatedGameBoard = [...previousGameBoard.map(innerArray => [...innerArray])]; // Creating a deep copy of the previous game board.
+  //     updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedGameBoard;
+  //   });
 
-    onSelectSquare();
-  }
+  //   onSelectSquare();
+  // }
 
   return (
     <ol id="game-board">
@@ -26,7 +24,7 @@ export default function GameBoard({ activePlayerSymbol, onSelectSquare }){
           <ol>
             {row.map((playerSymbol, colIndex) => ( // Rendering every cell of a row (every column of a row).
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}> {playerSymbol} </button>
+                <button onClick={onSelectSquare}> {playerSymbol} </button>
               </li>
             ))}
           </ol>
